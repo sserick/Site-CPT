7f25407b04ba3baaac6cdda2bf0944f35eb24033
s_erick_s <erick.silva.12348@gmail.com>
2020-10-28 12:48:09 -0300

    Projeto Base

---
+++ front-end/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-layout column justify-center align-center fill-height>
+      <p>
+        1) Olhem e pesquisem sobre a estrutura das Pastas NUXT no drive de
+        Projetos antes de iniciar
+      </p>
+      <p>2) Utilizem o Vuetify, contém vários componentes e scripts prontos</p>
+      <p>
+        3) Vejam os projetos antigos e se baseiem neles Ex: AfroCuritiba,
+        Humanidades, Site da Ecomp, etc...
+      </p>
+      <p>
+        4) Para pegar dados do backend, pesquisem sobre AXIOS e vejam o
+        <a
+          style="text-decoration: none;"
+          href="https://gitlab.com/ecomp-ufpr/cat-logos/front-enc"
+          target="_blank"
+          >Catálogo</a
+        >
+        de exemplos
+      </p>
+      <p>
+        6) Sigam o padrão de código estabelecido por Qualidade tiver
+      </p>
+      <!--link do doc qnd -->
+      <p>
+        5) **Não hesitem em perguntar, somos todos ECOMP**
+      </p>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
